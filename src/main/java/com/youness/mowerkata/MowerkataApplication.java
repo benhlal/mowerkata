edd9189d75792bc792e2b7421371bc4125ee1ef6
benhl <benhlalyounes@gmail.com>
2022-06-20 13:50:58 +0200

    project init

---
+++ src/main/java/com/youness/mowerkata/MowerkataApplication.java
@@ -0,0 +1,13 @@
+package com.youness.mowerkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MowerkataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MowerkataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youness/mowerkata/MowerkataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youness.mowerkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MowerkataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
